2cb63790c3bbd0c091e2997ebd374539a6cc17e8
PRitt842 <pamrittel@gmail.com>
2019-10-12 19:23:14 -0700

    2nd commit

---
+++ Code/Precip-station-markers.R
@@ -0,0 +1,13 @@
+## This code is to map stations with precip data to see their proximity to the Delta.
+library(tidyverse)
+library(ggplot2)
+library(tmap)
+library(tmaptools)
+library(sf)
+library(leaflet)
+sta.dat <- read_csv('Data/CDEC_Precip_Stations.csv')
+sta.dat
+# Make markers for each station
+leaflet(data = sta.dat) %>% addTiles() %>%
+  addCircleMarkers(lng = ~Longitude, lat = ~Latitude)
+# I can see that one station is far from study area so I need to remove it from the csv.
