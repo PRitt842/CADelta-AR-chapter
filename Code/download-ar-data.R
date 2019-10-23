21bcbfddd74249e828fb96dd51beee5d8b174095
Pamela Faye Rittelmeyer <prittelm@socs-stats.ucsc.edu>
2019-10-23 15:38:33 -0700

    commit download data

---
+++ Code/download-ar-data.R
@@ -0,0 +1,8 @@
+# download ar reanalysis data
+dir.create("data-raw/", F, T)
+
+# AR data
+download.file(
+  "https://ucla.app.box.com/v/ARcatalog/folder/16460775063",
+  "data-raw/globalARcatalog_MERRA2_1980-2019_v2.0.nc"
+)
