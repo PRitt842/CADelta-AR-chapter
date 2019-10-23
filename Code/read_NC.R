4326f5035fc8a70bede3960c93fd3389a6a2c1d7
PRitt842 <pamrittel@gmail.com>
2019-10-23 11:12:14 -0700

    nc code commit

---
+++ Code/read_NC.R
@@ -0,0 +1,8 @@
+library(RColorBrewer)
+library(lattice)
+library(ncdf4)
+library(chron)
+library(ncdf.tools)
+library(RNetCDF)
+# atmos-riv <- nc_open("globalARcatalog_MERRA2_1980-2019_v2.0.nc")
+# atmos-riv
