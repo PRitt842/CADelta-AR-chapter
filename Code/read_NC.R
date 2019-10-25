71fbe58e35d8220e4dab0c686beb3efdde45fd65
Pamela Faye Rittelmeyer <prittelm@socs-stats.ucsc.edu>
2019-10-25 14:08:40 -0700

    commit

--- Code/read_NC.R
+++ Code/read_NC.R
@@ -3,8 +3,22 @@ library(lattice)
 library(ncdf4)
 library(chron)
 library(RNetCDF)
-# atmos-riv <- nc_open("globalARcatalog_MERRA2_1980-2019_v2.0.nc")
-# atmos-riv
-# lat <- ncvar_get(atmos-riv, "lat")
-# lon <- ncvar_get(atmos-riv, "lon")
-# time <- ncvar_get(atmos-riv, "time")
+library(tidyverse)
+reanalys <- nc_open("data-raw/globalARcatalog_MERRA2_1980-2019_v2.0.nc")
+print(reanalys)
+# get lon and lat
+lat <- ncvar_get(reanalys, "lat")
+nlat <- dim(lat)
+head(lat)
+lon <- ncvar_get(reanalys, "lon")
+nlon <- dim(lon)
+head(lon)
+print(c(nlon,nlat))
+#get time
+time <- ncvar_get(reanalys, "time")
+head(time)
+tunits <- ncatt_get(reanlys,"time","units")
+nt <- dim(time)
+nt
+tunits
+nc_close(reanalys)
---
+++ Code/read_nc_as_raster.R
@@ -0,0 +1,2 @@
+library(raster)
+raster_layer <- raster("data-raw/globalARcatalog_MERRA2_1980-2019_v2.0.nc")
