4bbedb50512088bb9f9ec03b5a031c9d8dd57efb
Pamela Faye Rittelmeyer <prittelm@socs-stats.ucsc.edu>
2019-11-01 15:58:06 -0700

    commit brick

--- Code/download-ar-data.R
+++ Code/download-ar-data.R
@@ -1,6 +1,6 @@
-# download AR nc data - big file
+# download nc data 
 dir.create("data-raw/", F, T)
-# AR data
+# data
 download.file(
   "https://ucla.app.box.com/v/ARcatalog/folder/16460775063",
   "data-raw/globalARcatalog_MERRA2_1980-2019_v2.0.nc"
--- Code/read_NC.R
+++ Code/read_NC.R
@@ -3,12 +3,21 @@ library(raster)
 # open nc file
 ncin <- nc_open("data-raw/globalARcatalog_MERRA2_1980-2019_v2.0.nc")
 ncin
+varname <- names(ncin$var)
+varname
+b <- brick("data-raw/globalARcatalog_MERRA2_1980-2019_v2.0.nc", varname = 'lfloc', level=3)
+
+# b <- brick("data-raw/globalARcatalog_MERRA2_1980-2019_v2.0.nc", varname = 'lfloc', lvar=4)
+# NAvalue(b) <- 9e+20
+plot(b)
+# I get: Error in ncvar_get_inner(ncid2use, varid2use, nc$var[[li]]$missval, addOffset,  : 
+# Error: variable has 5 dims, but start has 4 entries.  They must match!
 # read raster
 ARras <- raster("data-raw/globalARcatalog_MERRA2_1980-2019_v2.0.nc", varname = 'lfloc', band = 1)
+# Again, I get: Warning message:
+  # In .rasterObjectFromCDF(x, type = objecttype, band = band, ...) :
+  # lfloc has more than 4 dimensions, I do not know what to do with these data
 ARras
-plot(ARras)
-
-b <- brick("data-raw/globalARcatalog_MERRA2_1980-2019_v2.0.nc", varname = 'lfloc')
 
 # get lon and lat
 lat <- ncvar_get(ncin, "lat")
