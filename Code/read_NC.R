8059c458366ad319bc13cb2b500f333ef1450fe9
Pamela Faye Rittelmeyer <prittelm@socs-stats.ucsc.edu>
2019-11-02 12:27:32 -0700

    commit trying to brick

--- Code/read_NC.R
+++ Code/read_NC.R
@@ -6,7 +6,7 @@ ncin
 varname <- names(ncin$var)
 varname
 b <- brick("data-raw/globalARcatalog_MERRA2_1980-2019_v2.0.nc", varname = 'lfloc', level=3)
-
+b
 # b <- brick("data-raw/globalARcatalog_MERRA2_1980-2019_v2.0.nc", varname = 'lfloc', lvar=4)
 # NAvalue(b) <- 9e+20
 plot(b)
@@ -18,7 +18,6 @@ ARras <- raster("data-raw/globalARcatalog_MERRA2_1980-2019_v2.0.nc", varname = '
   # In .rasterObjectFromCDF(x, type = objecttype, band = band, ...) :
   # lfloc has more than 4 dimensions, I do not know what to do with these data
 ARras
-
 # get lon and lat
 lat <- ncvar_get(ncin, "lat")
 nlat <- dim(lat)
@@ -34,4 +33,28 @@ tunits <- ncatt_get(ncin,"time","units")
 nt <- dim(time)
 nt
 tunits
+# convert time -- split the time units string into fields
+tustr <- strsplit(tunits$value, " ")
+tdstr <- strsplit(unlist(tustr)[3], "-")
+tmonth <- as.integer(unlist(tdstr)[2])
+tday <- as.integer(unlist(tdstr)[3])
+tyear <- as.integer(unlist(tdstr)[1])
+library(chron)
+chron(time,origin=c(tmonth, tday, tyear))
+# create raster brick
+b <- brick("data-raw/globalARcatalog_MERRA2_1980-2019_v2.0.nc", varname = 'lfloc')
+# b <- crop(b, extent(-121, -122, 37, 38))
+b <- crop(b, extent(144, 146, 14, 16))
+
+# get lfloc
+dname <- "lfloc"  
+# lf_array <- ncvar_get(ncin,dname)
+# dlname <- ncatt_get(ncin,dname,"long_name")
+# dunits <- ncatt_get(ncin,dname,"units")
+# fillvalue <- ncatt_get(ncin,dname,"_FillValue")
+# dim(lf_array)
+# replace netCDF fill values with NA's
+# lf_array[lf_array==fillvalue$value] <- NA
+# length(na.omit(as.vector(lf_array[,,1])))
 nc_close(ncin)
+
