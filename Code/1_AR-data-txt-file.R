57111d307a88c3652c982cb943ea7e20cfdba859
Pamela Faye Rittelmeyer <prittelm@socs-stats.ucsc.edu>
2019-11-22 18:03:16 -0800

    commit map - not working

--- Code/1_AR-data-txt-file.R
+++ Code/1_AR-data-txt-file.R
@@ -15,6 +15,7 @@ library(sf)
 library(readr)
 library(raster)
 library(sp)
+library(ggthemes)
 # Read atmospheric river global data
 global_ar <- read.csv('Data/globalAR_1980-2019.csv', header=TRUE)
 # head(global_ar)
@@ -24,18 +25,32 @@ global_ar <- read.csv('Data/globalAR_1980-2019.csv', header=TRUE)
 # str(global_ar)
 # Select columns we want
 colnames(global_ar)
-global_ar %>% 
-  select(Year, Month, Day, Hour, Total_IVT, Landfall_lon, Landfall_lat) %>%
-  filter(is.finite(Landfall_lon)) %>%
-  
+northam <- global_ar %>% 
+  select(Year, Month, Day, Hour, Equatorwd_end_lon, Equaterwd_end_lat, Polewd_end_lon, Polewd_end_lat, Total_IVT, Landfall_lon, Landfall_lat) %>%
+  filter(is.finite(Landfall_lon))
+# set bounding box for ARs we want to see
+usamap <- map_data('usa')
+usamap
+xlim <- c(-  127, -60) 
+ylim <- c(20, 50) 
+usbbox <- ggplot(usamap, aes('Landfall_lon','Landfall_lat')) +   
+  geom_map(map=usamap, aes(map_id=region), fill=3, color=1) +   
+  xlim(xlim)+   ylim(ylim)+   
+  coord_quickmap() 
+usbbox
+# map ARs in US
+armap <- ggplot(northam, aes(x = Landfall_lat, y = Landfall_lon)) +
+  borders("world", colour = "gray80", fill = "gray80", size = 0.3) +
+  geom_point(alpha = 0.3, size = 2, colour = "aquamarine3") 
+armap
 # summarise(TotalIVT = max(TotalIVT))
 
-
-# plot(global_ar)
-# Need to make a subset for a smaller file
 # Read the boundaries of the shapefile
 bbox <- read_sf(dsn = "Data/Legal_Delta_Boundary.shp") %>% st_bbox()
 bbox
 # Filter to a bounding box
 global_ar %>% 
   filter(Landfall_lat >= 37.62499 & Landfall_lat <= 38.58916 & Landfall_lon >= -121.94045 & Landfall_lon <-121.19670)
+# Remove all groups in the pipe
+ungroup()
+
